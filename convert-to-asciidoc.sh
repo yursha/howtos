7369caba46330fe42fe2a32c1e047207bfcbb4c2
Alex Yursha <alexyursha@gmail.com>
2017-11-24 19:42:55 -0800

    Prepare for md->adoc conversion

--- convert-to-asciidoc.sh
+++ convert-to-asciidoc.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env sh
 
-files=$(ls *.md)
+paths=$(find -name '*.md')
 
-for file in $files
+for path in $paths
 do
-	# ${parameter%word} shell parameter expansion
-	newfile="${file%.*}".adoc
-	echo $newfile
+	# new path will have extension changed
+	newpath="${path%.*}".adoc
+
+	# take last element of the path
+	title=$(basename "$path")
+
+	# trim file extension
+	title=${title%.*}
+
+	# replace hyphens with spaces
+	title=$(echo $title | sed 's/-/ /g')
+
+	# capitalize every word
+	title=$(echo $title | sed -e "s/\b\(.\)/\u\1/g")
+
+	toc="= $title"
+	toc="$toc\n:toc:\n:toc-placement!:\n\ntoc::[]\n\n"
+
+	echo $toc >> $newpath
+	pandoc -f markdown -t asciidoc $path >> $newpath
 done
